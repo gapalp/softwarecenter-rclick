86c6a0c015b1e799a9d500232c51c0ff4d899ab3
gapalp <gapalp@gapalp.net>
2013-05-21 21:11:22 -0400

    first commit

---
+++ softwarecenter-rclick-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Copying files..."
+cp Open\ Software\ Center $HOME/.gnome2/nautilus-scripts
+echo "Setting execute privileges..."
+chmod +x $HOME/.gnome2/nautilus-scripts/Open\ Software\ Center
